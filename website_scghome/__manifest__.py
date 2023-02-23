712f285bf83d568e726f8f841e6b3aa52e36cdb4
hewo-odoo <hewo@odoo.com>
2023-02-23 10:43:33 +0800

    [IMP] New pages & presets

--- website_scghome/__manifest__.py
+++ website_scghome/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'SCG home Theme',
     'category': 'Website/Theme',
-    'version': '16.0.0',
+    'version': '15.0.0',
     'depends': ['website_sale','website_blog'],
     'license': 'OEEL-1',
     'data': [
@@ -13,13 +13,16 @@
         'data/images.xml',
         # Pages
         'data/pages/home.xml',
+        'data/pages/contactus.xml',
         'data/pages/service_solutions.xml',
+        'data/pages/maintenance.xml',
         #Frontend
         'views/website_template.xml',
+        'views/website_blog.xml',
     ],
-    "demo": [
-        "demo/website_blog_demo.xml",
-    ],
+    # "demo": [
+    #     "demo/website_blog_demo.xml",
+    # ],
     'assets': {
         'web._assets_primary_variables': [
             'website_scghome/static/src/scss/primary_variables.scss',
